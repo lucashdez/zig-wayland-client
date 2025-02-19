e790d33338d29efe03c691f02241eb416e86a603
lucashdez <lukastpiano@gmail.com>
2025-02-19 17:13:41 +0000

    Changed things

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,11 @@
 const std = @import("std");
 
-
 pub fn create_wayland_socket(allocator: std.mem.Allocator) !std.posix.socket_t {
     const xdg_runtime_dir = std.posix.getenv("XDG_RUNTIME_DIR") orelse return error.NoXdgRuntime;
     const wayland_display = std.posix.getenv("WAYLAND_DISPLAY") orelse return error.NoWaylandDisplay;
     const socket_path = try std.fs.path.joinZ(allocator, &.{ xdg_runtime_dir, wayland_display });
     const socket = try std.posix.socket(std.posix.system.AF.UNIX, std.posix.system.SOCK.STREAM, 0);
-    var addr = std.posix.system.sockaddr.un {
+    var addr = std.posix.system.sockaddr.un{
         .path = undefined,
     };
     if (addr.path.len <= socket_path.len) {
@@ -17,7 +16,37 @@ pub fn create_wayland_socket(allocator: std.mem.Allocator) !std.posix.socket_t {
     return socket;
 }
 
+const WLHeader = packed struct {
+    id: u32,
+    op: u16,
+    size: u16,
+};
 
+const WlIds = struct {
+    const display = 1;
+};
+
+const DisplayOp = struct {
+    const get_registry = 1;
+};
+
+pub fn get_registry(socket: std.posix.socket_t, new_id: usize) !void {
+
+    const GetRegistryMessage = packed struct {
+        header: WLHeader,
+        new_id: usize,
+    };
+
+    const message = GetRegistryMessage {
+        .header = .{
+            .id = WlIds.display,
+            .op = DisplayOp.get_registry,
+            .size = @sizeOf(GetRegistryMessage),
+        },
+        .new_id = new_id,
+    };
+
+}
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;

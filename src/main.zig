b6281a2283f27d0f02329e0cb57cf080adbacdea
lucashdez <lukastpiano@gmail.com>
2025-03-09 19:19:32 +0000

    Got compositor

--- src/main.zig
+++ src/main.zig
@@ -4,11 +4,12 @@ const print = std.debug.print;
 const f_wayland_connection = @import("wayland_connection.zig");
 const f_events = @import("events.zig");
 const Display = @import("display.zig").Display;
+const Registry = @import("registry.zig").Registry;
+const Compositor = @import("compositor.zig").Compositor;
 const WaylandConnection = f_wayland_connection.WaylandConnection;
 const WlHeader = f_wayland_connection.WlHeader;
 const EventIt = f_events.EventIt;
 const EventDataParser = f_events.EventDataParser;
-const Registry = @import("registry.zig").Registry;
 
 pub const WaylandIdAllocator = struct {
     id: u32 = 3,
@@ -18,11 +19,17 @@ pub const WaylandIdAllocator = struct {
     }
 };
 
+const RawWayland  = struct {
+    compositor: ?Compositor,
+};
+
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
     const wl_connection: WaylandConnection = try WaylandConnection.init(allocator);
     const display = Display.init();
     const registry = try display.getRegistry(wl_connection.socket);
+    var compositor: ?Compositor = undefined;
+    var raw: RawWayland = RawWayland {.compositor = null};
 
     var buf: [4096]u8 = undefined;
     const response_l = try std.posix.read(wl_connection.socket, &buf);
@@ -35,35 +42,13 @@ pub fn main() !void {
             try display.handleEvent(event);
         } else if (event.header.id == registry.id) {
             const action = try registry.handleEvent(event);
-            switch (action) {
-                .global => |g| {
-                    if (std.mem.eql(u8, g.interface, "wl_compositor")) {
-                        const CompositorBindingMsg = packed struct {
-                            header: WlHeader,
-                            name: usize,
-                            new_id: usize,
-                        };
-                        const msg = CompositorBindingMsg{
-                            .header = .{
-                                .id = registry.id,
-                                .op = 0, // TODO: Change this
-                                .size = @sizeOf(CompositorBindingMsg),
-                            },
-                            .name = g.name,
-                            .new_id = ids.allocate(),
-                        };
-                        const written = try std.posix.write(wl_connection.socket, std.mem.asBytes(&msg));
-                        assert(written == @sizeOf(CompositorBindingMsg));
-                    } else {
-                        std.log.debug("{d}, {s}, {d}", .{ g.name, g.interface, g.version });
-                    }
-                },
-                .global_remove => |g| {
-                    std.log.debug("{any}", .{g});
-                },
+            compositor = try registry.bind_compositor(wl_connection.socket, action, ids.allocate());
+            if (compositor) |c| {
+                raw.compositor = c;
             }
         } else {
             print("{any}, {s}\n", .{ event.header, std.mem.trim(u8, event.data, " ") });
         }
     }
+    print("{any}", .{raw.compositor});
 }
--- src/registry.zig
+++ src/registry.zig
@@ -4,6 +4,7 @@ const EventIt = @import("events.zig").EventIt;
 const EventDataParser = @import("events.zig").EventDataParser;
 const Compositor = @import("compositor.zig").Compositor;
 const parseDataResponse = @import("meta.zig").parseDataResponse;
+const assert = std.debug.assert; 
 
 const RegistryOp = enum(u16) {
     bind = 0,
@@ -47,9 +48,36 @@ pub const Registry = struct {
             },
         }
     }
-  
-    pub fn bind_compositor(_: *const Registry, action: RegistryAction) !Compositor {
-            // TODO: Implement the binding of the compositor
 
+    pub fn bind_compositor(self: *const Registry, socket: std.posix.socket_t, action: RegistryAction, id: u32) !?Compositor {
+        switch (action) {
+            .global => |g| {
+                if (std.mem.eql(u8, g.interface, "wl_compositor")) {
+                    const CompositorBindingMsg = packed struct {
+                        header: WlHeader,
+                        name: usize,
+                        new_id: usize,
+                    };
+                    const msg = CompositorBindingMsg{
+                        .header = .{
+                            .id = self.id,
+                            .op = RegistryOp.bind.val(),
+                            .size = @sizeOf(CompositorBindingMsg),
+                        },
+                        .name = g.name,
+                        .new_id = id,
+                    };
+                    const written = try std.posix.write(socket, std.mem.asBytes(&msg));
+                    assert(written == @sizeOf(CompositorBindingMsg));
+                    return Compositor {.id = id};
+                } else {
+                    std.log.debug("{d}, {s}, {d}", .{ g.name, g.interface, g.version });
+                }
+            },
+            .global_remove => |g| {
+                std.log.debug("{any}", .{g});
+            },
+        }
+        return null;
     }
 };
